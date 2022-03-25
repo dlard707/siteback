fa3dc8178e0e60ba8cfabbf186318ceaa8ab6c43
DAVID ARIEL LARRONDA <dlard707@yahoo.com.ar>
2022-03-25 17:55:47 -0300

    2do commit variavel noticia e troca res.send x res.render()

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Importação Express
 const express = require('express');
+const noticias = require('./mockup')
 
 // criando um objeto na variável app
 const app = express();
@@ -10,10 +11,13 @@ app.use(express.static('public'));
 
 // Criando a nossa primeira rota
 app.get('/', (req, res) => {
-    res.send('home/index', {noticias: noticias.slice(0, 3)});
+    //mudar res.send() para res.render()
+    res.render('./home/index', {noticias: noticias.slice(0, 3)});
+   
 })
 //Criando a nossa segunda rota
 app.get('/noticias', (req, res) => {
+     //mudar res.send() para res.render()
     res.send('Todas as noticias');
 })
 
