2a71b3f02d2d1682f1d1a4311158a87b07cb52ba
DAVID ARIEL LARRONDA <dlard707@yahoo.com.ar>
2022-03-30 22:57:59 -0300

    frs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = require('./config/server');
 //coneão com o modulo de base de dados
 const db = require('./config/dbConnection');
 //Definindo a porta da aplicação
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // Criando a nossa primeira rota
 app.get('/', async (req, res) => {
